425549fa78b8ca8ca567d695966d2e9f1da2d189
Dinora15 <108556884+Dinora15@users.noreply.github.com>
2022-07-06 19:18:49 -0500

    Update Peliculas.jsx

--- src/Peliculas.jsx
+++ src/Peliculas.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-export function Pelicula (){
+export function Peliculas (){
     let {id, title}= useParams();
     return(
         <> 
